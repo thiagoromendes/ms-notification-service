d92c61b1a9367f5ff573bd8aced5c22c884d6b46
Thiago Mendes <thiago.mendes@evoluum.com.br>
2023-12-21 18:15:24 -0300

    chore: add connection to rabbitmq

--- src/mail/mail.conntroller.ts
+++ src/mail/mail.conntroller.ts
@@ -1,15 +1,52 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Logger, Query } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { Mail, Prisma } from '@prisma/client';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
 
-  @Get('getMail')
+  private readonly logger = new Logger(MailController.name);
+
+  @Get('get')
   async getMail(
     @Query('idUser') idUser: Prisma.MailWhereInput,
   ): Promise<Mail[] | null> {
     return await this.mailService.getMailByIdUser(idUser);
+  }
+
+  @MessagePattern('register')
+  async readRegisterPayment(@Payload() data, @Ctx() context: RmqContext) {
+    try {
+      this.logger.log(`data: ${JSON.stringify(data)}`);
+
+      const channel = context.getChannelRef();
+      const originalMesage = context.getMessage();
+
+      channel.ack(originalMesage);
+      return data;
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  @MessagePattern('confirmation')
+  async readConfimationPayment(@Payload() data, @Ctx() context: RmqContext) {
+    try {
+      this.logger.log(`data: ${JSON.stringify(data)}`);
+      const channel = context.getChannelRef();
+      const originalMesage = context.getMessage();
+
+      channel.ack(originalMesage);
+      return data;
+    } catch (ex) {
+      console.log(ex);
+    }
   }
 }
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Mail, Prisma } from '@prisma/client';
+
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
