bacd2315ff8b1b8fb37bb44e4879488c2d1a3359
Thiago Mendes <thiago.mendes@evoluum.com.br>
2023-12-21 21:59:17 -0300

    chore: adjusts to save notification

--- src/mail/mail.conntroller.ts
+++ src/mail/mail.conntroller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Logger, Query } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { Mail, Prisma } from '@prisma/client';
+import { Mail, MailType, Prisma } from '@prisma/client';
 import {
   Ctx,
   MessagePattern,
   Payload,
   RmqContext,
 } from '@nestjs/microservices';
+import DataMessage from './types/message';
 
 @Controller('mail')
 export class MailController {
@@ -22,29 +23,51 @@ export class MailController {
   }
 
   @MessagePattern('register')
-  async readRegisterPayment(@Payload() data, @Ctx() context: RmqContext) {
+  async readRegisterPayment(
+    @Payload() payload: any,
+    @Ctx() context: RmqContext,
+  ) {
     try {
-      this.logger.log(`data: ${JSON.stringify(data)}`);
+      this.logger.log(`data: ${JSON.stringify(payload)}`);
+
+      const dataMessage: DataMessage = JSON.parse(payload.data.notification);
 
       const channel = context.getChannelRef();
       const originalMesage = context.getMessage();
 
       channel.ack(originalMesage);
-      return data;
+
+      await this.mailService.sendMail(dataMessage, MailType.orderConfirmation);
+      await this.mailService.persistNotification(
+        dataMessage,
+        MailType.orderConfirmation,
+      );
     } catch (ex) {
       console.log(ex);
     }
   }
 
   @MessagePattern('confirmation')
-  async readConfimationPayment(@Payload() data, @Ctx() context: RmqContext) {
+  async readConfimationPayment(
+    @Payload() payload: any,
+    @Ctx() context: RmqContext,
+  ) {
     try {
-      this.logger.log(`data: ${JSON.stringify(data)}`);
+      const dataMessage: DataMessage = JSON.parse(payload.data.notification);
+
       const channel = context.getChannelRef();
       const originalMesage = context.getMessage();
 
       channel.ack(originalMesage);
-      return data;
+
+      await this.mailService.sendMail(
+        dataMessage,
+        MailType.paymentConfirmation,
+      );
+      await this.mailService.persistNotification(
+        dataMessage,
+        MailType.paymentConfirmation,
+      );
     } catch (ex) {
       console.log(ex);
     }
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Mail, Prisma } from '@prisma/client';
+import { Mail, MailType, Prisma } from '@prisma/client';
 
 import { PrismaService } from 'src/prisma.service';
+import DataMessage from './types/message';
 
 @Injectable()
 export class MailService {
@@ -9,5 +10,38 @@ export class MailService {
 
   async getMailByIdUser(where: Prisma.MailWhereInput): Promise<Mail[] | null> {
     return await this.prisma.mail.findMany({ where });
+  }
+
+  async sendMail(content: DataMessage, type: string) {
+    //implement method
+  }
+
+  async persistNotification(content: DataMessage, type: MailType) {
+    const data = {
+      idUser: content.idUser,
+      mailDestination: this.getDestination(content.idUser),
+      mailContent: this.makeContent(content.orderNumber, content.orderValue),
+      mailType: type,
+    };
+
+    await this.prisma.mail.create({
+      data: { ...data },
+    });
+  }
+
+  getDestination(idUser: string) {
+    switch (idUser) {
+      case '10':
+        return 'user@teste.com.br';
+
+      default:
+        return 'default@teste.com.br';
+    }
+  }
+
+  makeContent(orderNumber: number, orderValue: number) {
+    return `Número do pedido: ${orderNumber.toString()} /n/n
+      Valor do pedido: ${orderValue.toString()}
+      `;
   }
 }
